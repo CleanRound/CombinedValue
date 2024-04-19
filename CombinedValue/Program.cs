aec6c70aed3ea00049d8082b9c0d26b4c2d81727
Олег Луцик <oleglutsik.dev@gmail.com>
2024-04-19 19:19:21 +0300

    Добавьте файлы проекта.

---
+++ CombinedValue/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter four numbers:");
+        if (int.TryParse(Console.ReadLine(), out int num1) &&
+            int.TryParse(Console.ReadLine(), out int num2) &&
+            int.TryParse(Console.ReadLine(), out int num3) &&
+            int.TryParse(Console.ReadLine(), out int num4))
+        {
+            int formedNumber = num1 * 1000 + num2 * 100 + num3 * 10 + num4;
+            Console.WriteLine($"Formed number: {formedNumber}");
+        }
+        else
+        {
+            Console.WriteLine("Error: Invalid input.");
+        }
+    }
+}
